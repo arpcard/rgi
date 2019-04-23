ea8c30dadd65e2db2a44c7648dd911321c887cd8
Amos Raphenya <raphenar@mcmaster.ca>
2019-04-23 11:07:10 -0400

    adds tests for rgi bwt - issue#38

--- test.sh
+++ test.sh
@@ -9,8 +9,28 @@ wget -O card_data.tar.bz2 https://card.mcmaster.ca/latest/data
 mkdir -p card_data
 tar xvf card_data.tar.bz2 -C card_data
 
+# get latest card variants
+wget -O prevalence-v3.0.4.tar.gz https://card.mcmaster.ca/download/6/prevalence-v3.0.4.tar.gz
+mkdir -p card_variants 
+tar xvf prevalence-v3.0.4.tar.gz -C card_variants
+
+# create fasta files with annotations from card.json
+rgi card_annotation --input card_data/card.json
+
+data_version=`echo card_database_v*.fasta | sed 's/.*card_database_v\(.*\).fasta/\1/'`
+variants_version=`echo prevalence-v*.tar.gz | sed 's/.*prevalence-v\(.*\).tar.gz/\1/'`
+
+# create fasta files with annotations from variants
+rgi wildcard_annotation --input_directory card_variants --version "$variants_version" --card_json card_data/card.json
+
+# clean
+rgi clean --debug
+
+# load
+rgi load --card_json card_data/card.json --card_annotation card_database_v*.fasta --wildcard_index card_variants/index-for-model-sequences.txt --wildcard_version "$variants_version" --wildcard_annotation wildcard_database_v*.fasta --debug
+
 # for test_1.py
-cp card_data/card.json app/_data
+# cp card_data/card.json app/_data
 
 # for test_3.py
 cp card_data/card.json tests/inputs
---
+++ tests/test_4.py
@@ -0,0 +1,48 @@
+import pytest
+import os, json
+import subprocess as sp
+from app.MainBase import MainBase
+
+inputs = "inputs/"
+outputs = "outputs/"
+alignment_tool = "diamond"
+working_directory = os.getcwd()
+
+# Run all tests with
+# pytest test_4.py -v -rxs --color=auto --durations=0
+# or
+# pytest test_4.py -v -rxs --color=auto --durations=0 -k "protein"
+
+@pytest.fixture
+def rgi():
+	return MainBase(api=True)
+
+def run_rgi(rgi, read_one, read_two, aligner, threads, output_file):
+	parser = rgi.bwt_args()
+	rgi.bwt_run(parser.parse_args([
+		'--read_one', read_one,
+		'--read_two', read_two,
+		'--aligner', aligner,
+		'--threads', threads,
+		'--output_file', output_file,
+		'--clean',
+		'--debug'
+    ]))
+
+def check_gene(gene):
+    if gene == "tetQ":
+        return True
+    else:
+        return False
+
+def test_rgi_bwt(rgi):
+    read_one = "10_R1.fastq.gz"
+    read_two = "10_R2.fastq.gz"
+    filename = "output"
+    output_file = os.path.join(working_directory,outputs,"{}".format(filename))
+    run_rgi(rgi, os.path.join(working_directory,inputs,read_one), os.path.join(working_directory,inputs,read_two), "bowtie2", "3" ,output_file)
+    gene = sp.getoutput("cat {gene_summary_output} | cut -f1 | grep {amr_gene}".format(
+        gene_summary_output="{}.gene_mapping_data.txt".format(output_file),
+        amr_gene = "tetQ"
+    ))
+    assert check_gene(gene) == True
