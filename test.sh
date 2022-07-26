1313394d3d0efe62b63c935c11e9d26b887a52e0
Amos Raphenya <raphenar@mcmaster.ca>
2022-07-26 13:36:27 -0400

    updates commands

--- test.sh
+++ test.sh
@@ -19,13 +19,13 @@ tar xf card_data.tar.bz2 -C card_data
 echo "=================================== DOWNLOAD CARD VARIANTS DATA ==================================="
 # get latest card variants
 wget -O prevalence-v3.0.4.tar.gz --no-check-certificate https://card.mcmaster.ca/download/6/prevalence-v3.0.4.tar.gz
-mkdir -p card_variants 
+mkdir -p card_variants
 tar xf prevalence-v3.0.4.tar.gz -C card_variants
 
 echo "=================================== CARD CANONICAL ANNOTATIONS ==================================="
 # create fasta files with annotations from card.json
-echo "$cmd python3 ./rgi card_annotation --input card_data/card.json"
-python3 ./rgi card_annotation --input card_data/card.json
+echo "$cmd rgi card_annotation --input card_data/card.json"
+rgi card_annotation --input card_data/card.json
 
 echo "=================================== VERSIONS ==================================="
 
@@ -36,31 +36,31 @@ echo "$cmd variants_version: $variants_version"
 
 echo "=================================== CARD VARIANTS ANNOTATIONS ==================================="
 # create fasta files with annotations from variants
-echo "$cmd python3 ./rgi wildcard_annotation --input_directory card_variants --version '$variants_version' --card_json card_data/card.json"
-python3 ./rgi wildcard_annotation --input_directory card_variants --version "$variants_version" --card_json card_data/card.json
+echo "$cmd rgi wildcard_annotation --input_directory card_variants --version '$variants_version' --card_json card_data/card.json"
+rgi wildcard_annotation --input_directory card_variants --version "$variants_version" --card_json card_data/card.json
 
 echo "=================================== CLEAN OLD DATABASES ==================================="
 # clean
-echo "$cmd ./rgi clean --debug"
-python3 ./rgi clean --debug
+echo "$cmd rgi clean --debug"
+rgi clean --debug
 
 echo "=================================== LOAD DATABASES ==================================="
 # load
-echo "$cmd python3 ./rgi load --card_json card_data/card.json --card_annotation card_database_v${data_version}.fasta --wildcard_index card_variants/index-for-model-sequences.txt --wildcard_version '$variants_version' --wildcard_annotation wildcard_database_v${variants_version}.fasta --debug"
-python3 ./rgi load --card_json card_data/card.json --card_annotation card_database_v${data_version}.fasta --wildcard_index card_variants/index-for-model-sequences.txt --wildcard_version "$variants_version" --wildcard_annotation wildcard_database_v${variants_version}.fasta --debug
+echo "$cmd rgi load --card_json card_data/card.json --card_annotation card_database_v${data_version}.fasta --wildcard_index card_variants/index-for-model-sequences.txt --wildcard_version '$variants_version' --wildcard_annotation wildcard_database_v${variants_version}.fasta --debug"
+rgi load --card_json card_data/card.json --card_annotation card_database_v${data_version}.fasta --wildcard_index card_variants/index-for-model-sequences.txt --wildcard_version "$variants_version" --wildcard_annotation wildcard_database_v${variants_version}.fasta --debug
 
 echo "=================================== CHECK LOADED DATABASES ==================================="
 # check database
-echo "$cmd python3 ./rgi database -v --all"
-python3 ./rgi database -v --all
+echo "$cmd rgi database -v --all"
+rgi database -v --all
 
 echo "=================================== COPY DATA & INPUTS ==================================="
 # for test_1.py
-echo "$cmd card_data/card.json app/_data"
+echo "$cmd cp card_data/card.json app/_data"
 cp card_data/card.json app/_data
 
 # for test_3.py
-echo "$cmd card_data/card.json tests/inputs"
+echo "$cmd cp card_data/card.json tests/inputs"
 cp card_data/card.json tests/inputs
 
 echo "=================================== RUN TESTS ==================================="
@@ -80,4 +80,3 @@ echo "=================================== DONE =================================
 
 # exit with the exitcode thrown by pytest
 exit $?
-
