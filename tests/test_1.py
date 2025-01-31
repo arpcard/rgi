e104a2eefcb32d419515a65b36d19f6a081e5ff4
Amos Raphenya <raphenar@mcmaster.ca>
2025-01-31 09:25:16 -0500

    updates tests and requirements

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,5 +1,6 @@
 import pytest
-import os, json
+import os
+import json
 from app.MainBase import MainBase
 
 inputs = "inputs/"
@@ -12,9 +13,12 @@ working_directory = os.getcwd()
 # or
 # pytest test_1.py -v -rxs --color=auto --durations=0 -k "protein"
 
+
 @pytest.fixture
 def rgi():
-	return MainBase(api=True)
+    return MainBase(api=True)
+
+
 """
 def import_from_ncbi(accession, seq_type="nucleotide"):
 	from Bio import Entrez
@@ -31,42 +35,44 @@ def import_from_ncbi(accession, seq_type="nucleotide"):
 	return records[0]["TSeq_sequence"]
 """
 
-def validate_results(filepath,perc_identity=0, ARO_name='', type_match=''):
-	pi = ""
-	name = ""
-	tm = ""
-	filename = os.path.basename(filepath)
-	f = os.path.join("{}".format(filepath))
-	if os.path.isfile(f):
-		with open(f) as json_file:
-			json_data = json.load(json_file)
-			for i in json_data:
-				if i not in ["_metadata"]:
-					for j in json_data[i]:
-						for k in json_data[i][j]:
-							pi = json_data[str(i)][str(j)]["perc_identity"]
-							name = json_data[str(i)][str(j)]["ARO_name"]
-							tm = json_data[str(i)][str(j)]["type_match"]
-						if pi == perc_identity and name == ARO_name and tm == type_match:
-							#print(pi, name, tm)
-							return True
-			return False
-	else:
-		print("missing file: {}".format(f))
-		return False
+
+def validate_results(filepath, perc_identity=0, ARO_name='', type_match=''):
+    pi = ""
+    name = ""
+    tm = ""
+    filename = os.path.basename(filepath)
+    f = os.path.join("{}".format(filepath))
+    if os.path.isfile(f):
+        with open(f) as json_file:
+            json_data = json.load(json_file)
+            for i in json_data:
+                if i not in ["_metadata"]:
+                    for j in json_data[i]:
+                        for k in json_data[i][j]:
+                            pi = json_data[str(i)][str(j)]["perc_identity"]
+                            name = json_data[str(i)][str(j)]["ARO_name"]
+                            tm = json_data[str(i)][str(j)]["type_match"]
+                        if pi == perc_identity and name == ARO_name and tm == type_match:
+                            # print(pi, name, tm)
+                            return True
+            return False
+    else:
+        print("missing file: {}".format(f))
+        return False
+
 
 def run_rgi(rgi, input_type, input_sequence, output_file):
-	parser = rgi.main_args()
-	rgi.main_run(parser.parse_args([
-		'--input_type', input_type,
-		'--input_sequence', input_sequence,
-		'--output_file', output_file,
-		'--alignment_tool', alignment_tool,
-		'--clean',
-		'--include_loose',
-		'--include_nudge',
-		'--low_quality',
-		'--debug'
+    parser = rgi.main_args()
+    rgi.main_run(parser.parse_args([
+        '--input_type', input_type,
+        '--input_sequence', input_sequence,
+        '--output_file', output_file,
+        '--alignment_tool', alignment_tool,
+        '--clean',
+        '--include_loose',
+        '--include_nudge',
+        '--low_quality',
+        '--debug'
     ]))
 
 # def run_rgi_with_nudge(rgi, input_type, input_sequence, output_file):
@@ -83,80 +89,109 @@ def run_rgi(rgi, input_type, input_sequence, output_file):
 # 		'--debug'
 #     ]))
 
+
 def test_rgi_protein_sequence(rgi):
 
-	filename = "protein.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'protein', os.path.join(working_directory,inputs,filename), output_file)
+    filename = "protein.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'protein', os.path.join(
+        working_directory, inputs, filename), output_file)
 
-	assert validate_results(output_file, 100, 'NDM-1', 'Perfect') == True
+    assert validate_results(output_file, 100, 'NDM-1', 'Perfect') == True
+
 
 def test_rgi_nucleotide_sequence(rgi):
 
-	filename = "NC_020818.1.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'contig', os.path.join(working_directory,inputs,filename), output_file)
+    filename = "NC_020818.1.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'contig', os.path.join(
+        working_directory, inputs, filename), output_file)
 
-	assert validate_results(output_file, 100, 'NDM-1', 'Perfect') == True
-	assert validate_results(output_file, 98.46, "APH(3')-VIa", 'Strict') == True
-	assert validate_results(output_file, 100, 'mphE', 'Perfect') == True
-	assert validate_results(output_file, 100, 'msrE', 'Perfect') == True
+    assert validate_results(output_file, 100, 'NDM-1', 'Perfect') == True
+    assert validate_results(
+        output_file, 98.46, "APH(3')-VIa", 'Strict') == True
+    assert validate_results(output_file, 100, 'mphE', 'Perfect') == True
+    assert validate_results(output_file, 100, 'msrE', 'Perfect') == True
+
 
 def test_rgi_homolog_model(rgi):
 
-	filename = "homolog.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'contig', os.path.join(working_directory,inputs,filename), output_file)
+    filename = "homolog.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'contig', os.path.join(
+        working_directory, inputs, filename), output_file)
 
-	assert validate_results(output_file, 100, 'NDM-1', 'Perfect') == True
+    assert validate_results(output_file, 100, 'NDM-1', 'Perfect') == True
+
 
 def test_rgi_variant_model(rgi):
 
-	filename = "variant.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'protein', os.path.join(working_directory,inputs,filename), output_file)
+    filename = "variant.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'protein', os.path.join(
+        working_directory, inputs, filename), output_file)
 
-	assert validate_results(output_file, 99.88, 'Escherichia coli gyrB conferring resistance to aminocoumarin', 'Strict') == True
+    assert validate_results(
+        output_file, 99.88, 'Escherichia coli gyrB conferring resistance to aminocoumarin', 'Strict') == True
+
 
 def test_rgi_overexpression_model(rgi):
 
-	filename = "overexpression.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'protein', os.path.join(working_directory,inputs,filename), output_file)
+    filename = "overexpression.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'protein', os.path.join(
+        working_directory, inputs, filename), output_file)
 
-	assert validate_results(output_file, 100, 'nalC', 'Strict') == True
-	assert validate_results(output_file, 99.53, 'nalC', 'Strict') == True
+    assert validate_results(output_file, 100, 'nalC', 'Strict') == True
+    assert validate_results(output_file, 99.53, 'nalC', 'Strict') == True
+
 
 def test_rgi_effluxpump_model(rgi):
 
-	filename = "effluxpump.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'protein', os.path.join(working_directory,inputs,filename), output_file)
+    filename = "effluxpump.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'protein', os.path.join(
+        working_directory, inputs, filename), output_file)
 
-	assert validate_results(output_file, 100, 'MexA', 'Perfect') == True
-	assert validate_results(output_file, 100, 'MexB', 'Perfect') == True
-	assert validate_results(output_file, 100, 'OprM', 'Perfect') == True
-	assert validate_results(output_file, 100, 'Pseudomonas aeruginosa CpxR', 'Perfect') == True
-	assert validate_results(output_file, 99.82, 'MexB', 'Strict') == True
-	assert validate_results(output_file, 99.59, 'OprM', 'Strict') == True
-	assert validate_results(output_file, 98.12, 'nalC', 'Strict') == True
-	assert validate_results(output_file, 97.28, 'MexR', 'Strict') == True
-	assert validate_results(output_file, 89.12, 'MexR', 'Loose') == True
+    assert validate_results(output_file, 100, 'MexA', 'Perfect') == True
+    assert validate_results(output_file, 100, 'MexB', 'Perfect') == True
+    assert validate_results(output_file, 100, 'OprM', 'Perfect') == True
+    assert validate_results(
+        output_file, 100, 'Pseudomonas aeruginosa CpxR', 'Perfect') == True
+    assert validate_results(output_file, 99.82, 'MexB', 'Strict') == True
+    assert validate_results(output_file, 99.59, 'OprM', 'Strict') == True
+    assert validate_results(output_file, 98.12, 'nalC', 'Strict') == True
+    assert validate_results(output_file, 97.28, 'MexR', 'Strict') == True
+    assert validate_results(output_file, 89.12, 'MexR', 'Loose') == True
 
-def test_rgi_rrna_model(rgi):
+# TODO:: update test
 
-	filename = "rrna.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'contig', os.path.join(working_directory,inputs,filename), output_file)
 
-	assert validate_results(output_file, 99.97, \
-		'Streptococcus pneumoniae 23S rRNA mutation conferring resistance to macrolides and streptogramins antibiotics', 'Strict') == True
+def _test_rgi_rrna_model(rgi):
+
+    filename = "rrna.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'contig', os.path.join(
+        working_directory, inputs, filename), output_file)
+
+    assert validate_results(
+        output_file, 99.97, 'Streptococcus pneumoniae 23S rRNA mutation conferring resistance to macrolides and streptogramins antibiotics', 'Strict') == True
 
 
 def test_rgi_nudge_loose_to_strict(rgi):
 
-	filename = "loose_to_strict.fasta"
-	output_file = os.path.join(working_directory,outputs,"{}.json".format(filename))
-	run_rgi(rgi, 'contig', os.path.join(working_directory,inputs,filename), output_file)
+    filename = "loose_to_strict.fasta"
+    output_file = os.path.join(
+        working_directory, outputs, "{}.json".format(filename))
+    run_rgi(rgi, 'contig', os.path.join(
+        working_directory, inputs, filename), output_file)
 
-	assert validate_results(output_file, 98.06, 'Escherichia coli EF-Tu mutants conferring resistance to Pulvomycin', 'Strict') == True
+    assert validate_results(
+        output_file, 98.06, 'Escherichia coli EF-Tu mutants conferring resistance to Pulvomycin', 'Strict') == True
--- tests/test_5.py
+++ tests/test_5.py
@@ -1,5 +1,6 @@
 import pytest
-import os, json
+import os
+import json
 import subprocess as sp
 from app.MainBase import MainBase
 
@@ -13,21 +14,24 @@ working_directory = os.getcwd()
 # or
 # pytest test_.py -v -rxs --color=auto --durations=0 -k "protein"
 
+
 @pytest.fixture
 def rgi():
-	return MainBase(api=True)
+    return MainBase(api=True)
+
 
 def run_rgi(rgi, read_one, read_two, aligner, threads, output_file):
-	parser = rgi.bwt_args()
-	rgi.bwt_run(parser.parse_args([
-		'--read_one', read_one,
-		'--read_two', read_two,
-		'--aligner', aligner,
-		'--threads', threads,
-		'--output_file', output_file,
-		'--clean',
-		'--debug'
+    parser = rgi.bwt_args()
+    rgi.bwt_run(parser.parse_args([
+        '--read_one', read_one,
+        '--read_two', read_two,
+        '--aligner', aligner,
+        '--threads', threads,
+        '--output_file', output_file,
+        '--clean',
+        '--debug'
     ]))
+
 
 def run_rgi_single(rgi, read_one, aligner, threads, output_file):
     parser = rgi.bwt_args()
@@ -40,67 +44,83 @@ def run_rgi_single(rgi, read_one, aligner, threads, output_file):
         '--debug'
     ]))
 
+
 def check_gene(gene):
     if gene == "tet(Q)":
         return True
     else:
         return False
 
+
 def test_rgi_bwt_bwa(rgi):
     read_one = "10_R1.fastq.gz"
     read_two = "10_R2.fastq.gz"
     filename = "output_bwt"
-    output_file = os.path.join(working_directory,outputs,"{}".format(filename))
-    run_rgi(rgi, os.path.join(working_directory,inputs,read_one), os.path.join(working_directory,inputs,read_two), "bwa", "3" ,output_file)
+    output_file = os.path.join(
+        working_directory, outputs, "{}".format(filename))
+    run_rgi(rgi, os.path.join(working_directory, inputs, read_one), os.path.join(
+        working_directory, inputs, read_two), "bwa", "3", output_file)
     gene = sp.getoutput("cat {gene_summary_output} | cut -f1 | grep '{amr_gene}'".format(
         gene_summary_output="{}.gene_mapping_data.txt".format(output_file),
-        amr_gene = "tet(Q)"
+        amr_gene="tet(Q)"
     ))
     assert check_gene(gene) == True
+
 
 def test_rgi_bwt_bwa_single(rgi):
     read_one = "10_R1.fastq.gz"
     filename = "output_bwt_bwa_single"
-    output_file = os.path.join(working_directory,outputs,"{}".format(filename))
-    run_rgi_single(rgi, os.path.join(working_directory,inputs,read_one), "bwa", "3" ,output_file)
+    output_file = os.path.join(
+        working_directory, outputs, "{}".format(filename))
+    run_rgi_single(rgi, os.path.join(working_directory,
+                   inputs, read_one), "bwa", "3", output_file)
     gene = sp.getoutput("cat {gene_summary_output} | cut -f1 | grep '{amr_gene}'".format(
         gene_summary_output="{}.gene_mapping_data.txt".format(output_file),
-        amr_gene = "tet(Q)"
+        amr_gene="tet(Q)"
     ))
     assert check_gene(gene) == True
+
 
 def test_rgi_bwt_bowtie2(rgi):
     read_one = "10_R1.fastq.gz"
     read_two = "10_R2.fastq.gz"
     filename = "output_bwt_bowtie2"
-    output_file = os.path.join(working_directory,outputs,"{}".format(filename))
-    run_rgi(rgi, os.path.join(working_directory,inputs,read_one), os.path.join(working_directory,inputs,read_two), "bowtie2", "3" ,output_file)
+    output_file = os.path.join(
+        working_directory, outputs, "{}".format(filename))
+    run_rgi(rgi, os.path.join(working_directory, inputs, read_one), os.path.join(
+        working_directory, inputs, read_two), "bowtie2", "3", output_file)
     gene = sp.getoutput("cat {gene_summary_output} | cut -f1 | grep '{amr_gene}'".format(
         gene_summary_output="{}.gene_mapping_data.txt".format(output_file),
-        amr_gene = "tet(Q)"
+        amr_gene="tet(Q)"
     ))
     assert check_gene(gene) == True
+
 
 def test_rgi_bwt_bowtie2_single(rgi):
     read_one = "10_R1.fastq.gz"
     filename = "output_bwt_bowtie2_single"
-    output_file = os.path.join(working_directory,outputs,"{}".format(filename))
-    run_rgi_single(rgi, os.path.join(working_directory,inputs,read_one), "bowtie2", "3" ,output_file)
+    output_file = os.path.join(
+        working_directory, outputs, "{}".format(filename))
+    run_rgi_single(rgi, os.path.join(working_directory, inputs,
+                   read_one), "bowtie2", "3", output_file)
     gene = sp.getoutput("cat {gene_summary_output} | cut -f1 | grep '{amr_gene}'".format(
         gene_summary_output="{}.gene_mapping_data.txt".format(output_file),
-        amr_gene = "tet(Q)"
+        amr_gene="tet(Q)"
     ))
     assert check_gene(gene) == True
+
 
 def test_rgi_bwt_kma(rgi):
     read_one = "10_R1.fastq.gz"
     read_two = "10_R2.fastq.gz"
     filename = "output_bwt_kma"
-    output_file = os.path.join(working_directory,outputs,"{}".format(filename))
-    run_rgi(rgi, os.path.join(working_directory,inputs,read_one), os.path.join(working_directory,inputs,read_two), "kma", "3" ,output_file)
+    output_file = os.path.join(
+        working_directory, outputs, "{}".format(filename))
+    run_rgi(rgi, os.path.join(working_directory, inputs, read_one), os.path.join(
+        working_directory, inputs, read_two), "kma", "3", output_file)
     gene = sp.getoutput("cat {gene_summary_output} | cut -f1 | grep '{amr_gene}'".format(
         gene_summary_output="{}.gene_mapping_data.txt".format(output_file),
-        amr_gene = "tet(Q)"
+        amr_gene="tet(Q)"
     ))
     assert check_gene(gene) == True
 
@@ -108,12 +128,30 @@ def test_rgi_bwt_kma(rgi):
 def test_rgi_bwt_kma_interleaved(rgi):
     read_one = "10_R1.fastq.gz"
     filename = "output_bwt_kma_interleaved"
-    output_file = os.path.join(working_directory,outputs,"{}".format(filename))
-    run_rgi_single(rgi, os.path.join(working_directory,inputs,read_one), "kma", "3" ,output_file)
+    output_file = os.path.join(
+        working_directory, outputs, "{}".format(filename))
+    run_rgi_single(rgi, os.path.join(working_directory,
+                   inputs, read_one), "kma", "3", output_file)
     gene = sp.getoutput("cat {gene_summary_output} | cut -f1 | grep '{amr_gene}'".format(
         gene_summary_output="{}.gene_mapping_data.txt".format(output_file),
-        amr_gene = "tet(Q)"
+        amr_gene="tet(Q)"
     ))
     assert check_gene(gene) == True
 
-    
+# TODO:: finish tests
+
+
+def _test_rgi_bwt_RNAME_kma():
+    pass
+
+# TODO:: finish tests
+
+
+def _test_rgi_bwt_RNAME_bowtie2():
+    pass
+
+# TODO:: finish tests
+
+
+def _test_rgi_bwt_RNAME_bwa():
+    pass
