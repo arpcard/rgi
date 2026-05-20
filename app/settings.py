d8a3f3699537de5da1c1cc15266352af39b064fe
Amos Raphenya <raphenar@mcmaster.ca>
2026-05-20 10:52:22 -0400

    update version

--- app/settings.py
+++ app/settings.py
@@ -41,9 +41,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(level)
 
 # detailed log
-# formatter = logging.Formatter('%(levelname)s %(asctime)s : (%(filename)s::%(funcName)s::%(lineno)d) : %(message)s')
+formatter = logging.Formatter(
+    '%(levelname)s %(asctime)s : (%(filename)s::%(funcName)s::%(lineno)d) : %(message)s')
 # basic log
-formatter = logging.Formatter('%(levelname)s %(asctime)s : %(message)s')
+# formatter = logging.Formatter('%(levelname)s %(asctime)s : %(message)s')
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
@@ -53,7 +54,7 @@ logger.addHandler(stream_handler)
 LOCAL_DATABASE = os.path.join(os.getcwd(), "localDB")
 
 APP_NAME = "Resistance Gene Identifier"
-SOFTWARE_VERSION = "6.0.7"
+SOFTWARE_VERSION = "6.0.8"
 SOFTWARE_SUMMARY = 'Use the Resistance Gene Identifier to predict resistome(s) from protein or nucleotide \
 data based on homology and SNP models. Check https://card.mcmaster.ca/download for software and data updates. \
 Receive email notification of monthly CARD updates via the CARD Mailing List \
